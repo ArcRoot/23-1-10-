8347632211f07124cd42817531d5b92e3f795bb3
20221938 <117703433+20221938@users.noreply.github.com>
2023-07-17 20:04:52 +0900

    Add files via upload

---
+++ Item_booster.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_booster : MonoBehaviour
+{
+    private float boostingTimer = 0f;
+    private bool isBoosting = false;
+    private PlayerMove boostingPlayer; // PlayerMove 컴포넌트를 참조할 변수
+    private void StartBoosting(Collider2D collision)
+    {
+        isBoosting = true;
+        boostingTimer = 0f;
+        boostingPlayer = collision.gameObject.GetComponent<PlayerMove>();
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("booster");
+        StartBoosting(collision); // 부스팅 시작
+    }
+    void Update()
+    {
+        if (isBoosting)
+        {
+            boostingTimer += Time.deltaTime; // 경과한 시간을 누적
+
+            if (boostingTimer >= 3f) // 3초 동안 작동했을 때
+            {
+                isBoosting = false;
+                boostingTimer = 0f;
+            }
+            else
+            {
+                boostingPlayer.boosting(); // 부스팅 함수 호출
+            }
+        }
+    }
+    void Start()
+    {
+
+    }
+}
---
+++ Item_ignore.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_ignore : MonoBehaviour
+{
+    private float ignoringTimer = 0f;
+    private bool isignoring = false;
+    private SpriteRenderer spriteRenderer;
+    private Collider2D collision;
+
+    private void StartIgnoring()
+    {
+        isignoring = true;
+        ignoringTimer = 0f;
+        spriteRenderer = GetComponent<SpriteRenderer>(); // 스프라이트 렌더러 컴포넌트를 가져옴
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("ignore"))
+        {
+            Debug.Log("ignore");
+            other.gameObject.SetActive(false); //아이템 사라짐
+            collision = other.GetComponent<Collider2D>();
+            StartIgnoring(); // 무적 시작
+        }
+    }
+
+    private void Update()
+    {
+        if (isignoring)
+        {
+            ignoringTimer += Time.deltaTime; // 경과한 시간을 누적
+            spriteRenderer.color = new Color(1, 1, 1, 0.5f);
+            Physics2D.IgnoreCollision(collision, GetComponent<Collider2D>(), true);
+
+            if (ignoringTimer >= 3f) // 3초 동안 작동했을 때
+            {
+                isignoring = false;
+                ignoringTimer = 0f;
+                spriteRenderer.color = new Color(1, 1, 1, 1); //무적 타임 종료(원래대로)
+                Physics2D.IgnoreCollision(collision, GetComponent<Collider2D>(), false);
+            }
+        }
+    }
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>(); // 스프라이트 렌더러 컴포넌트를 가져옴
+    }
+}
