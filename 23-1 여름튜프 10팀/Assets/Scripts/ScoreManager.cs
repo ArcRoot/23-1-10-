33f6ec20479515dd788ce53adeca59b8dd1a9f04
CAUKimJisu <127710709+CAUKimJisu@users.noreply.github.com>
2023-07-17 20:04:11 +0900

    Add files via upload

--- 23-1 여름튜프 10팀/Assets/Scripts/ScoreManager.cs
+++ 23-1 여름튜프 10팀/Assets/Scripts/ScoreManager.cs
@@ -2,26 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ScoreManager : MonoBehaviour
 {
-    int score = 0;
-    float timer = 0f;
-    float scoreInterval = 1f;
+    public static ScoreManager instance;
+    private int score = 0;
+    private float timer = 0f;
+    private float scoreInterval = 10f;
+    private bool scoreEffectActive = false;
 
-    void Update()
+    public TextMesh scoreText; // 또는 public TMPro.TextMeshProUGUI scoreText; (Unity UI Text를 사용하는 경우)
+
+    private void Awake()
     {
-        timer += Time.deltaTime;
-
-        if (timer >= scoreInterval)
+        // ScoreManager 인스턴스 생성 또는 중복 방지
+        if (instance == null)
         {
-            timer = 0f;
-            IncreaseScore();
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 
-    void IncreaseScore()
+    private void FixedUpdate()
     {
-        score += 10;
-        Debug.Log("Score: " + score);
+        timer += Time.fixedDeltaTime;
+
+        while (timer >= scoreInterval && !scoreEffectActive)
+        {
+            timer -= scoreInterval;
+            IncreaseScore(scoreEffectActive ? 2 : 1);
+        }
     }
+
+    public void IncreaseScore(int amount)
+    {
+        score += amount;
+        Debug.Log("Score: " + score);
+
+        // UI Text 업데이트
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + score.ToString();
+        }
+    }
+
+    public void ActivateScoreEffect(int amount)
+    {
+        scoreEffectActive = true;
+        IncreaseScore(amount); // 아이템 효과로 인한 두 배 점수 증가
+    }
+
+    public void DeactivateScoreEffect(int amount)
+    {
+        scoreEffectActive = false;
+        IncreaseScore(-amount); // 효과 종료로 다시 원래의 점수로 돌리기
+    }
+
 }
